52facb0688758c0b65db23687c20d3a5b9ec9f1f
tinsygit <tinsyding@gmail.com>
2023-12-08 08:00:47 +0800

    python

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-# Python程序，用于创建用于删除图标缓存的批处理文件
+username = input("Please enter your computer username: ")
 
-# 提示用户输入用户名
-username = input("请输入你的计算机用户名：")
-
-# 带有用户名占位符的文本模板
 batch_text = """
 @echo off
 echo Deleting Icon Cache...
-del /f /q "C:\\Users\\[username]\\AppData\\Local\\IconCache.db"
+del /f /q "C:\\Users\\[yourusername]\\AppData\\Local\\IconCache.db"
 echo Restarting Explorer...
 taskkill /f /im explorer.exe
 start explorer.exe
 echo Done.
 """
 
-# 将占位符替换为用户输入的用户名
-batch_text = batch_text.replace("[username]", username)
+batch_text = batch_text.replace("[yourusername]", username)
 
-# 将修改后的文本保存为'main.bat'
 with open("main.bat", "w") as file:
     file.write(batch_text)
